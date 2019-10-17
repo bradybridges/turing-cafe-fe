43afb617097c5f47293274be484eb5341f3bc918
Brady Bridges <bradyjbridges@gmail.com>
2019-10-17 10:13:48 -0600

    Add ReservationForm, add func to make reservation

---
+++ src/ReservationForm/ReservationForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './ReservationForm.css'
+
+class ReservationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: 0,
+    }
+  }
+
+  updateReservationDetails = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+    if(e.target.name === 'number') {
+      this.setState({number: Number(e.target.value)})
+    }
+  }
+
+  isFormComplete = () => {
+    const isNameGood = this.state.name !== '';
+    const isDateGood = this.state.date !== '';
+    const isTimeGood = this.state.time !== '';
+    const isNumberGood = this.state.number !== '';
+
+    if(isNameGood && isDateGood && isTimeGood && isNumberGood) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  makeReservation = (e) => {
+    e.preventDefault();
+    const reservation = {
+      name: this.state.name,
+      date: this.state.date,
+      time: this.state.time,
+      number: this.state.number
+    };
+    this.props.addReservation(reservation);
+  }
+
+  render() {
+    return (
+      <form>
+        <input 
+          onChange={this.updateReservationDetails} 
+          type='text' 
+          placeholder='Name' 
+          name='name'
+        />
+        <input 
+          onChange={this.updateReservationDetails} 
+          type='text' 
+          placeholder='Date (mm/dd)' 
+          name='date'
+        />
+        <input 
+          onChange={this.updateReservationDetails} 
+          type='text' 
+          placeholder='Time' 
+          name='time'
+        />
+        <input 
+          onChange={this.updateReservationDetails} 
+          type='text' 
+          placeholder='Number of guests'
+          name='number' 
+        />
+        {!this.isFormComplete() && <button disabled>Make Reservation</button>}
+        {this.isFormComplete() && <button onClick={this.makeReservation} >Make Reservation</button>}
+      </form>
+    );
+  }
+}
+
+export default ReservationForm;
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const fetchReservations = () => {
   return fetch('http://localhost:3001/api/v1/reservations')
     .then( resp => resp.json());
 }
+
+export const insertReservation = reservation => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(reservation),
+    headers: { 'Content-Type': 'application/json' }
+  }
+  return fetch('http://localhost:3001/api/v1/reservations', options)
+    .then(resp => resp.json());
+
+}
